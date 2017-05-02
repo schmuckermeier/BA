021dc945cd31690ff04c0be13c9f5aef6200ea82
Sophie <sophie.v.schmuckermeier@campus.tu-berlin.de>
2017-05-02 15:35:48 +0200

    implemented basic file chooser window

--- BA-thesis/src/view/view.java
+++ BA-thesis/src/view/view.java
@@ -1,23 +1,45 @@
 package view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class view {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Test");
+		JFrame frame = new JFrame("Bachelor Thesis - Sophie Schmuckermeier - 2017");
 		frame.setVisible(true);
 		frame.setSize(500, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		JButton button = new JButton("Load");
-		panel.add(button);
+		JLabel header = new JLabel("Choose File for prediction:");
+		panel.add(header);
+		JButton loadButton = new JButton("Load File");
+		panel.add(loadButton);
+		loadButton.addActionListener(new LoadAction());
 
-		JButton button2 = new JButton("Exit");
-		panel.add(button2);
+		JButton exitButton = new JButton("Exit");
+		panel.add(exitButton);
+		exitButton.addActionListener(new ExitAction());
+
 	}
 
+	static class LoadAction implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser chooser = new JFileChooser();
+			chooser.showOpenDialog(null);
+		}
+	}
+
+	static class ExitAction implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	}
 }
